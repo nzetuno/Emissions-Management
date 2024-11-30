9f2e7d632df50c83dd3a7d43f92f335ba6e2eb16
nzetuno <nzetocha@unomaha.edu>
2024-11-29 22:11:42 -0600

    Update all_user.ts

--- pages/api/all_user.ts
+++ pages/api/all_user.ts
@@ -1,4 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+i// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from "@/model/db";
 
@@ -6,6 +6,6 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const EmissionSources = await prisma.user.findMany()
+    const EmissionSources = await prisma.EmissionSources.findMany()
     return res.status(200).json(EmissionSources)
 }
