2643a5abf821a19b8206ce486bc032e4a8b014f8
nzetuno <nzetocha@unomaha.edu>
2024-11-29 21:20:47 -0600

    Update all_user.ts

--- pages/api/all_user.ts
+++ pages/api/all_user.ts
@@ -6,6 +6,6 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const users = await prisma.user.findMany()
-    return res.status(200).json(users)
+    const EmissionSources = await prisma.EmissionSource.findMany()
+    return res.status(200).json(EmissionSources)
 }
