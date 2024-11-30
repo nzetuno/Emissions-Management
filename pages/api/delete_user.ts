d81242e810ef678dd8a51ec00a1d1cb8d7818473
nzetuno <nzetocha@unomaha.edu>
2024-11-29 22:10:23 -0600

    Update delete_user.ts

--- pages/api/delete_user.ts
+++ pages/api/delete_user.ts
@@ -8,7 +8,7 @@ export default async function handler(
     res: NextApiResponse
 ) {
     try {
-        await prisma.EmissionSource.delete({
+        await prisma.user.delete({
             where: req.body,
     });
         res.status(200).json('success');
