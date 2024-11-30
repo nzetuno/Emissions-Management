9db5da908499132be4234cf91e69d333b4a7c0cd
nzetuno <nzetocha@unomaha.edu>
2024-11-29 22:10:10 -0600

    Update create_user.ts

--- pages/api/create_user.ts
+++ pages/api/create_user.ts
@@ -9,7 +9,7 @@ export default async function handler(
 ) {
     console.log('1111', req.body);
     try {
-        const EmissionSource = await prisma.EmissionSource.create({ data: req.body });
+        const user = await prisma.user.create({ data: req.body });
         res.status(200).json(EmissionSource);
     } catch (e) {
         res.status(400).json(e);
